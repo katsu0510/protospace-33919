91d1c7227bba32bfb98a6c3f0f0a3af29ec1f8b9
katsu0510 <katsu0510310@gmail.com>
2021-02-04 16:34:36 +0900

    修正点の変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-
+  before_action :set_prototype, only: [:edit, :show, :update, :destroy]
   before_action :move_to_index, except: [:index, :show]
 
 
@@ -7,9 +7,11 @@ class PrototypesController < ApplicationController
     @prototypes = Prototype.all
   end
 
+
   def new
     @prototype = Prototype.new
   end
+
 
   def create
     @prototype = Prototype.new(prototype_params)
@@ -18,36 +20,38 @@ class PrototypesController < ApplicationController
     else
       render :new
     end
-
-    
   end
+
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
-    
   end
+
 
   def edit
-    @prototype = Prototype.find(params[:id])
+    unless current_user == @prototype.user
+      redirect_to action: :index
+    end
   end
 
+
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
-      redirect_to prototype_path
+    if @prototype.update(prototype_params)
+       redirect_to prototype_path
     else
       render :edit
     end
-    
-    
   end
 
+
   def destroy
-    prototype = Prototype.find(params[:id])
-    prototype.destroy
+    unless current_user == @prototype.user
+      redirect_to action: :index
+    end
+    @prototype.destroy
     redirect_to root_path
+    
   end
   
 
@@ -56,7 +60,11 @@ class PrototypesController < ApplicationController
 
   private
 
-  def move_to_index
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index  
     unless user_signed_in?
       redirect_to action: :index
     end
@@ -66,3 +74,5 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
+
+
